09890e42aa67bb07dca15bfc32a4caf3da2dc578
notpdrkk <pedrohsrgpp@gmail.com>
2026-03-04 11:23:25 -0300

    remoção de itens

--- moduloPHP/projetoPHP/functions.php
+++ moduloPHP/projetoPHP/functions.php
@@ -10,16 +10,21 @@ $usuarios = [
 
 $tarefas = [
     [
+        "id" => 1,
         "descricao" => "Terminar feature",
         "prioridade" => "alta",
         "prazo" => "7 dias",
         "concluida" => false
-    ]
+    ],
 ];
+
+$ehLogado = false;
 
 function loginMenu()
 {
-    global $usuarios;
+    global $usuarios, $ehLogado;
+
+    limpaTela();
 
     echo "Por favor, realize o login!\n";
     $email = readline("Email: ");
@@ -43,8 +48,9 @@ function loginMenu()
 
 function mainMenu()
 {
+    limpaTela();
     echo "Seja bem vindo(a) ao Sistema!\n";
-    echo "1 - Criar tarefa\n2 - Listar\n3 - Buscar\n4 - Editar\n5 - Remover\n6 - Estatísticas\n7 - Cadastrar usuário (somente admin)\n8 - Deslogar\n";
+    echo "1 - Criar tarefa\n2 - Listar\n3 - Buscar\n4 - Editar\n5 - Remover\n6 - Estatísticas\n7 - Cadastrar usuário\n8 - Deslogar\n";
     $escolha = readline("Escolha uma opção: ");
 
     switch ($escolha) {
@@ -79,6 +85,7 @@ function mainMenu()
 
         case 8:
             echo "Deslogando";
+
             loginMenu();
             break;
 
@@ -93,22 +100,41 @@ function criarTarefa()
 {
     global $tarefas;
 
+    limpaTela();
+
     echo "Criação de tarefas: \n";
+
+    $id = 1;
+
+    foreach ($tarefas as $tarefa) {
+        if ($tarefa["id"] === $id) {
+            $id += 1;
+        }
+    }
 
     $descTarefa = readline("Insira uma descrição: ");
     $prioridadeTarefa = readline("Insira uma prioridade: ");
     $prazoTarefa = readline("Insira um prazo (em dias): ");
     $estaConcluida = false;
+    $tarefas[] = [
+        "id" => $id,
+        "descricao" => $descTarefa,
+        "prioridade" => $prioridadeTarefa,
+        "prazo" => $prazoTarefa,
+        "concluida" => $estaConcluida
+    ];
 
-    $tarefas[] = ["descricao" => $descTarefa, "prioridade" => $prioridadeTarefa, "prazo" => $prazoTarefa, "concluida" => $estaConcluida];
+    echo "Deseja criar outra tarefa?\n1 - Sim\n2 - Não\n";
+    $escolha = readline("Sua escolha: ");
 
-    $escolha = readline("Deseja criar outra tarefa?\n1 - Sim\n2 - Não");
     echo ($escolha == 1) ? criarTarefa() : mainMenu();
 }
 
 function listarTarefas()
 {
     global $tarefas;
+
+    limpaTela();
 
     foreach ($tarefas as $tarefa) {
         echo "Tarefa: ", $tarefa["descricao"], " | ", "Prazo: ", $tarefa["prazo"], "\n";
@@ -136,16 +162,51 @@ function listarTarefas()
 
 function buscarTarefa()
 {
-    echo "buscando tarefa";
+    global $tarefas;
+
+    limpaTela();
+
+    $busca = readline("Procure uma tarefa pelo id: ");
+
+    foreach ($tarefas as $tarefa => $valor) {
+        if ($busca == $valor['id']) {
+            echo "\nId: " . $valor["id"];
+            echo "\nDescrição: " . $valor["descricao"];
+            echo "\nPrioridade: " . $valor["prioridade"];
+            echo "\nPrazo: " . $valor["prazo"];
+            echo "\nConcluida: " . $valor["concluida"];
+            echo "\n";
+        }
+        return ['index' => $tarefa, 'valor' => $valor];
+    }
 }
 function editarTarefa()
 {
-    echo "editando tarefa";
+    global $tarefas;
+
+    limpaTela();
+
+    $busca = buscarTarefa();
+
+    $index = $busca['index'];
+
 }
 function removerTarefa()
 {
-    echo "removendo tarefa";
+    global $tarefas;
+
+    limpaTela();
+
+    $busca = buscarTarefa();
+
+    $index = $busca['index'];
+    unset($tarefas[$index]);
+
+    array_values($tarefas);
+    print_r($busca);
+    mainMenu();
 }
+
 function mostrarEstatisticas()
 {
     echo "estatisticas";
@@ -154,7 +215,7 @@ function mostrarEstatisticas()
 function cadastrarUsuario()
 {
     global $usuarios;
-    
+
     $novoNome = readline("Digite seu nome: ");
     $novoEmail = readline("Digite seu email: ");
     $novaSenha = readline("Digite sua senha: ");
@@ -162,4 +223,9 @@ function cadastrarUsuario()
     $usuarios[] = ["nome" => $novoNome, "email" => $novoEmail, "senha" => $novaSenha];
     echo "Usuário cadastrado com sucesso!\n";
     mainMenu();
+}
+
+function limpaTela()
+{
+    echo (PHP_OS_FAMILY == 'Windows') ? system('cls') : system('clear');
 }
